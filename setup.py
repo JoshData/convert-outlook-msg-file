d4a5944abaf90193b2f12dba36ab23d00f780d03
Rodrigo Salvador <rcsalvador@users.noreply.github.com>
2019-09-16 20:49:04 -0300

    Include dependencies by requirements.txt (#10)

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+install_requires = [
+    'compoundfiles',
+    'compressed_rtf',
+]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='convert-outlook-msg-file',
+    version='0.1.0',
+    description='Parse Microsoft Outlook MSG files',
+    author='Joshua Tauberer',
+    author_email='jt@occams.info',
+    url='https://github.com/JoshData/convert-outlook-msg-file',
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
